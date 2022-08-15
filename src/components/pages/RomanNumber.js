f06f9b31bf1f4c2515c23287bc71d5ad920d938d
Ludmi Lopez <ludmit@outlook.com>
2022-08-15 19:07:02 -0300

    understand-error-building-roman-number

--- src/components/pages/RomanNumber.js
+++ src/components/pages/RomanNumber.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-const RomanNumber = () => {
-  return <div><h1>Roman Number Converter</h1></div> ;
-};
-
+function RomanNumber() {
+  var romanToNumber ={
+    M: 1000,
+    CM:900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    VI: 4,
+    I:1
+  };
+  return (
+    <div className='home' id='theme'>Roman Number Converter</div>
+  )
+}
 export default RomanNumber;
