b1e47f3265d791a8286d43c84b3eb58f6a251225
Ludmi Lopez <ludmit@outlook.com>
2022-10-21 17:43:16 -0300

    thiss

--- src/components/pages/Components/Todo.js
+++ src/components/pages/Components/Todo.js
@@ -23,10 +23,7 @@ function Todo({todos, completeTodo, removeTodo, updatedTodos}) {
 
   return todos.map((todo, index) =>
     <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-            {todo.text}
-        </div>
-        <div classame='icons'>
+    <div className='icons'>
             <RiCloseCircleLine 
             onClick={() => removeTodo(todo.id)}
             className='delete-icon' />
@@ -35,6 +32,10 @@ function Todo({todos, completeTodo, removeTodo, updatedTodos}) {
             className='edit-icon'
             />
         </div>
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+            {todo.text}
+        </div>
+        
     </div>
   )
 }
